f2942c7913dd70373f73d372c22ce3ab404ee60c
ghadah <gh.gadahd01@gmail.com>
2024-08-26 13:56:42 +0300

    checking the entered values

--- src/Program.cs
+++ src/Program.cs
@@ -2,34 +2,46 @@
 
     static void Main () {
 
-        Console.WriteLine("Wich Unit You Want to Convert, Press (C or F) ");
-        char tempUnit = Convert.ToChar(Console.ReadLine());
-        
-        Console.WriteLine("Enter a Temperature Degree: ");
-        double tempDegree = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter a temperature and its unit (C or F): ");
+        string temp = Console.ReadLine();
+
+        string [] tempSplitting = temp.Split(" ");
+        int tempDegree = Convert.ToInt32(tempSplitting[0]);
+        string tempUnit = tempSplitting[1];
        
        Console.WriteLine(TempConvert (tempDegree , tempUnit));
         
     }
-        public static string TempConvert (double tempDegree , char tempUnit){
+        public static string TempConvert (int tempDegree , string tempUnit){
+
+ // checking entered value is 2 character or not 45 
+        int size = tempDegree.ToString().Length;
+        if (size == 2){
 
 // converting fahrenheit to celsius formula C = (F - 32) × 5/9 
-        if (tempUnit == 'F'){
+        if (tempUnit == "F"){
 
-            double celsiusDegree = (tempDegree - 32) * 5/9;
+            double celsiusDegree = ( tempDegree - 32) * 5.0/9.0;
             return  $"Converted: {tempDegree} F = {celsiusDegree} C";
 
         } 
  // converting celsius to fahrenheit formula F = (9/5) C + 32
-        else if (tempUnit == 'C') {
+        else if (tempUnit == "C") {
 
-            double fahrenheitDegree = ((9.0/5) * tempDegree) + 32;
+            double fahrenheitDegree = ((9.0/5.0d) * tempDegree) + 32;
             return  $"Converted: {tempDegree} C = {fahrenheitDegree} F";
         }
         else{
             return "Sorry!";
         }
-
+    
+ } else{
+    return "Sorry!";
+ 
     }
 }
+
+}
+
+
 
