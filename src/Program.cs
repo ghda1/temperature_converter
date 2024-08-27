941836515e07b67e21ee8828b38837513a7120fa
ghadah <gh.gadahd01@gmail.com>
2024-08-27 10:45:47 +0300

    Add exceptions

--- src/Program.cs
+++ src/Program.cs
@@ -3,28 +3,52 @@
     static void Main () {
 
         while (true){
+
         Console.WriteLine("Enter a temperature and its unit (C or F), or type 'Quit' to exit:");
         string temp = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(temp)){
+
+        Console.WriteLine("invalid inputs! Enter a temperature and its unit (C or F), or type 'Quit' to exit:");
+        }else {
+
+        
 
         if (temp.Trim().ToLower() == "quit"){
             break;
         }
 
         string [] tempSplitting = temp.Split(" ");
-        int tempDegree = Convert.ToInt32(tempSplitting[0]);
-        string tempUnit = tempSplitting[1];
-       
-       Console.WriteLine(TempConvert (tempDegree , tempUnit));
-        
-        }}
-        public static string TempConvert (int tempDegree , string tempUnit){
 
  // checking entered value is 2 character or not  
-        int size = tempDegree.ToString().Length;
-        if (size == 2){
+        if (tempSplitting.Length != 2){
+
+            Console.WriteLine("Invalid length! Please enter a numeric temperature and units.");
+        }
+
+        bool isValid = int.TryParse(tempSplitting[0], out int tempDegree);
+        if (isValid == false){
+            Console.WriteLine("Invalid input! Please enter a numeric temperature."); // make sure that the first index is a number
+        }
+        
+        string tempUnit = tempSplitting[1];
+
+        try{
+            Console.WriteLine(TempConvert (tempDegree , tempUnit));
+        }
+        catch (ArgumentException e) {
+            Console.WriteLine(e.Message);
+
+        }
+        
+        }
+    }
+    }
+        public static string TempConvert (int tempDegree , string tempUnit){
+
+        tempUnit = tempUnit.ToUpper(); // handling lowerCase inputs 
 
 // converting fahrenheit to celsius formula C = (F - 32) × 5/9 
-        tempUnit = tempUnit.ToUpper();
         if (tempUnit == "F"){
 
             double celsiusDegree = ( tempDegree - 32) * 5.0/9.0;
@@ -38,15 +62,10 @@
             return  $"Converted: {tempDegree} C = {fahrenheitDegree} F";
         }
         else{
-            return "Invalid units!!";
+            // handling wrong units
+            throw new ArgumentException("Invalid units! Please enter 'C' for Celsius or 'F' for Fahrenheit.");
         }
     
- } else{
-    return "Invalid length!";
- 
-    }
 }
 
 }
-
-
